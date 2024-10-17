6a0f78ea4c3fe576e058e4a6bbedbccdc0f161cb
r.kawano <r.kawano@genbasupport.com>
2024-10-17 09:50:01 +0900

    get_notesメソッドをload_notesメソッド、set_notesメソッドをsave_notesメソッドに修正

--- quicknote.rb
+++ quicknote.rb
@@ -10,11 +10,11 @@ def generate_escape_character(text)
   Rack::Utils.escape_html(text)
 end
 
-def get_notes(file_path)
+def load_notes(file_path)
   File.open(file_path) { |f| JSON.parse(f.read) }
 end
 
-def set_notes(file_path, notes)
+def save_notes(file_path, notes)
   File.write(file_path, JSON.dump(notes))
 end
 
@@ -23,7 +23,7 @@ get '/' do
 end
 
 get '/quicknote' do
-  @notes = get_notes(FILE_PATH)
+  @notes = load_notes(FILE_PATH)
   erb :index
 end
 
@@ -32,7 +32,7 @@ get '/quicknote/new' do
 end
 
 get '/quicknote/:id' do
-  notes = get_notes(FILE_PATH)
+  notes = load_notes(FILE_PATH)
   content = notes[params[:id]]
   if content
     text = content['content'].split
@@ -46,33 +46,33 @@ end
 
 post '/quicknote' do
   content = params[:content]
-  notes = get_notes(FILE_PATH)
+  notes = load_notes(FILE_PATH)
   id = notes.empty? ? '1' : (notes.keys.map(&:to_i).max + 1).to_s
   notes[id] = { 'content' => content }
-  set_notes(FILE_PATH, notes)
+  save_notes(FILE_PATH, notes)
 
   redirect '/quicknote'
 end
 
 get '/quicknote/:id/edit' do
-  notes = get_notes(FILE_PATH)
+  notes = load_notes(FILE_PATH)
   @content = generate_escape_character(notes[params[:id]]['content'])
   erb :edit
 end
 
 patch '/quicknote/:id' do
   content = params[:content]
-  notes = get_notes(FILE_PATH)
+  notes = load_notes(FILE_PATH)
   notes[params[:id]] = { 'content' => content }
-  set_notes(FILE_PATH, notes)
+  save_notes(FILE_PATH, notes)
 
   redirect "/quicknote/#{params[:id]}"
 end
 
 delete '/quicknote/:id' do
-  notes = get_notes(FILE_PATH)
+  notes = load_notes(FILE_PATH)
   notes.delete(params[:id])
-  set_notes(FILE_PATH, notes)
+  save_notes(FILE_PATH, notes)
 
   redirect '/quicknote'
 end
